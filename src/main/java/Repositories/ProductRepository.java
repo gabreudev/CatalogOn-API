a53347c53e5a275be21834de3498f5a9614e0cba
gabriel <biel0alves0@gmail.com>
2024-06-06 11:54:10 -0300

    feat: add product repositoty

---
+++ src/main/java/Repositories/ProductRepository.java
@@ -0,0 +1,6 @@
+package Repositories;
+import Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
