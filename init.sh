b78d9d9f85314090c5999a7c54033f3f7b3c7e00
gabreudev <biel0alves0@gmail.com>
2024-07-27 22:37:27 -0300

    feat: configuring init cql file

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Esperar até que o Cassandra esteja pronto para aceitar conexões
 until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do
-    sleep 5;
-    echo "Waiting for cassandra...";
+  sleep 5;
+  echo "Waiting for cassandra...";
 done
 
-echo "Creating keyspace and table..."
+echo "Cassandra is up - executing commands..."
+
+# Criar keyspace e tabela
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS mykeyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS mykeyspace.product ( id UUID PRIMARY KEY, nome text, descricao text, preco float, img text);"
